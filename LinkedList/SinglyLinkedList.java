f0d4def81a228fc420c84c1295756b6b677f87a6
saneenkp <saneenkp2000@gmail.com>
2020-08-26 23:55:56 +0530

    all deletion functions added

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -5,6 +5,7 @@
        REMOVE HEAD    O(1)
        REMOVE TAIL    O(n)
        REMOVE POS     O(n)*/ 
+
 import java.util.Scanner;
 class SinglyLinkedList {
 
@@ -14,31 +15,56 @@ class SinglyLinkedList {
         Scanner scanner = new Scanner(System.in);
         int ch = 0;
         do{
-            System.out.println("\nEnter\n1. to add elements\n2. to show");
+            System.out.println("\nEnter\n1. to add elements\n2. to show\n3. to insert At End\n4. to Insert At position\n5. to delete at end\n6. To delete at beginnnig\n7. to delete at position\n8. ge the size of list \n9. exit. ");
             ch = scanner.nextInt();
             switch(ch)
             {
-               case 1 :  System.out.println("Enter element");
-                         int extra = scanner.nextInt();
-                         linkedlist.Addtothebeginning(extra);
-                         break;
-   
-               case 2 : linkedlist.show();
-                        break;
-               case 3 :  System.out.println("Enter position of the element ");
-                           int r = scanner.nextInt();
-                          linkedlist.AddtotheEnd(r);
+                case 1 :  System.out.println("Enter element");
+                          int extra = scanner.nextInt();
+                          linkedlist.Addtothebeginning(extra);
+                          linkedlist.show();
                           break;
-                case 4 : 
-                            System.out.println("Enter element");
-                            int el = scanner.nextInt();                
-                            System.out.println("Enter position of the element ");
-                           int p = scanner.nextInt();
+   
+                case 2 :  linkedlist.show();
+                          break;
 
-                          linkedlist.Addatposition(el , p);
+                case 3 :  System.out.println("Enter position of the element ");
+                          int end = scanner.nextInt();
+                          linkedlist.AddtotheEnd(end);
+                          linkedlist.show();
+        
+                          break;
                 
-            } 
-        }while(ch!=5);
+                case 4 : System.out.println("Enter element");
+                         int el = scanner.nextInt();                
+                         System.out.println("Enter position of the element ");
+                         int p = scanner.nextInt();
+                         linkedlist.Addatposition(el , p);
+                         linkedlist.show();
+                         break;
+
+                case 5 : linkedlist.DeleteAtEnd();
+                         linkedlist.show();
+                         break;         
+                
+                case 6 : linkedlist.DeleteAtBeginning();
+                         linkedlist.show();
+                         break;
+
+                case 7 : System.out.println("Enter position of the element ");
+                         int dp = scanner.nextInt();
+                         linkedlist.DeleteAtPosition(dp);
+                         linkedlist.show();
+                         break;
+
+                case 8 : System.out.println("SIZE = " + linkedlist.getsize());
+                         break;
+
+                default : System.out.println("\t\t\t !!! INVALID INPUT PRESS 9 TO EXIT !!!");
+
+            }
+
+        }while(ch!=9);
     }
 
 }
@@ -56,9 +82,7 @@ class Node {
     /*String value;
     float value;
     char value;
-
-
-   
+    
     Node(float value , Node next)
     {
         this.value = value;
@@ -127,39 +151,87 @@ class LinkedList {
         size++;
         if(p == 1)
         {
-            new_value.next = head;
-            head = new_value;
+            Addtothebeginning(value);
         }
         else if(p == size)
         {
-            Node tmp = head;
-            while(tmp.next != null)
-            {
-                tmp = tmp.next;
-            }
-            tmp.next = new_value;
-            tail = new_value;
+            AddtotheEnd(value);
+        }
+        else if(p > size)
+        {
+            System.out.println("\t\t\t  INVALID POSITION ");
         }
         else
         {
-            Node tmp1 = head;
-            Node tmp2 = head.next;
+            Node tmp = head;
             int i = 2;
             while(i < p)
             {
-                tmp2 = tmp2.next;
-                tmp1 = tmp1.next;
+                tmp = tmp.next;
                 i++;
             }
-            tmp1.next = new_value;
-            new_value.next = tmp2;
+            new_value.next = tmp.next;
+            tmp.next = new_value;
         }
     }
 
+    public void DeleteAtEnd()
+    {
+        Node tmp1 = head;
+        while(tmp1.next.next != null)
+        {
+            tmp1 = tmp1.next;
+        }
+        tmp1.next = null;
+        tail = tmp1;
+        size--;
+    }
+
+    public void DeleteAtBeginning()
+    {
+        head = head.next;
+        size--;
+    }
+
+    public void DeleteAtPosition(int p)
+    {
+        size--;
+        if(p == 1)
+        {
+           DeleteAtBeginning();
+        }
+        else if(p == size)
+        {
+
+          DeleteAtEnd();
+        }
+        else if(p > size)
+        {
+            System.out.println("\t\t\t  INVALID POSITION ");
+        }
+        else
+        {
+            System.out.println("pos gone");
+            Node tmp = head;
+            int i = 2;
+            while(i < p)
+            {
+                tmp = tmp.next;
+                i++;
+            }
+            Node container = tmp.next.next;
+            tmp.next = container;
+        }
+
+       
+    }
+
+    public int getsize()
+    {return size;}
 
     public void add(String value)
     {
-
+        
     }
     public void add(char value)
     {
@@ -169,8 +241,6 @@ class LinkedList {
     {
 
     }
-
-
 
     public void show()
     {
